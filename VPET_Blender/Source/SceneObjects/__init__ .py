2225525f68110d71252f22576a5e8bb4b60e4df1
AlexandruSchwartz <alexandru.schwartz@filmakademie.de>
2024-07-09 18:06:33 +0200

    License for Blender

--- VPET_Blender/Source/SceneObjects/__init__ .py
+++ VPET_Blender/Source/SceneObjects/__init__ .py
@@ -1,32 +1,34 @@
 """
------------------------------------------------------------------------------
-This source file is part of VPET - Virtual Production Editing Tools
-http://vpet.research.animationsinstitut.de/
-http://github.com/FilmakademieRnd/VPET
-
-Copyright (c) 2021 Filmakademie Baden-Wuerttemberg, Animationsinstitut R&D Lab
-
-This project has been initiated in the scope of the EU funded project
-Dreamspace under grant agreement no 610005 in the years 2014, 2015 and 2016.
-http://dreamspaceproject.eu/
-Post Dreamspace the project has been further developed on behalf of the
-research and development activities of Animationsinstitut.
-
-The VPET component Blender Scene Distribution is intended for research and development
-purposes only. Commercial use of any kind is not permitted.
-
-There is no support by Filmakademie. Since the Blender Scene Distribution is available
-for free, Filmakademie shall only be liable for intent and gross negligence;
-warranty is limited to malice. Scene DistributiorUSD may under no circumstances
-be used for racist, sexual or any illegal purposes. In all non-commercial
-productions, scientific publications, prototypical non-commercial software tools,
-etc. using the Blender Scene Distribution Filmakademie has to be named as follows:
-“VPET-Virtual Production Editing Tool by Filmakademie Baden-Württemberg,
-Animationsinstitut (http://research.animationsinstitut.de)“.
-
-In case a company or individual would like to use the Blender Scene Distribution in
-a commercial surrounding or for commercial purposes, software based on these
-components or any part thereof, the company/individual will have to contact
-Filmakademie (research<at>filmakademie.de).
------------------------------------------------------------------------------
+TRACER Scene Distribution Plugin Blender
+ 
+Copyright (c) 2024 Filmakademie Baden-Wuerttemberg, Animationsinstitut R&D Labs
+https://research.animationsinstitut.de/tracer
+https://github.com/FilmakademieRnd/TracerSceneDistribution
+ 
+TRACER Scene Distribution Plugin Blender is a development by Filmakademie
+Baden-Wuerttemberg, Animationsinstitut R&D Labs in the scope of the EU funded
+project MAX-R (101070072) and funding on the own behalf of Filmakademie
+Baden-Wuerttemberg.  Former EU projects Dreamspace (610005) and SAUCE (780470)
+have inspired the TRACER Scene Distribution Plugin Blender development.
+ 
+The TRACER Scene Distribution Plugin Blender is intended for research and
+development purposes only. Commercial use of any kind is not permitted.
+ 
+There is no support by Filmakademie. Since the TRACER Scene Distribution Plugin
+Blender is available for free, Filmakademie shall only be liable for intent
+and gross negligence; warranty is limited to malice. TRACER Scene Distribution
+Plugin Blender may under no circumstances be used for racist, sexual or any
+illegal purposes. In all non-commercial productions, scientific publications,
+prototypical non-commercial software tools, etc. using the TRACER Scene
+Distribution Plugin Blender Filmakademie has to be named as follows: 
+"TRACER Scene Distribution Plugin Blender by Filmakademie
+Baden-Württemberg, Animationsinstitut (http://research.animationsinstitut.de)".
+ 
+In case a company or individual would like to use the TRACER Scene Distribution
+Plugin Blender in a commercial surrounding or for commercial purposes,
+software based on these components or  any part thereof, the company/individual
+will have to contact Filmakademie (research<at>filmakademie.de) for an
+individual license agreement.
+ 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
